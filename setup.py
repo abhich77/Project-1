aa327e4253f7cd73c6150c7d7550011251e8d5a3
Abhiraj Chaudhary <abhirajc.ug23.ee@nitp.ac.in>
2026-07-07 00:30:25 +0530

    project structure ready,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhiraj Chaudhary",
+    author_email="abhirajch.77@gmail.com",
+    packages=find_packages()
+
+)

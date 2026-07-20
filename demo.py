9493c17575a56246dea8488532d985011543181b
Abhiraj Chaudhary <abhirajc.ug23.ee@nitp.ac.in>
2026-07-20 21:13:18 +0530

    fixed bugs-3

--- demo.py
+++ demo.py
@@ -2,11 +2,11 @@ from src.logger import logging
 
 logging.debug("testing logger")
 
-from src.exception import MYException
+from src.exception import MyException
 import sys
 
 try:
     a=1+"z"
 except Exception as e:
     logging.info(e)
-    raise MYException(e,sys) from e
+    raise MyException(e,sys) from e
